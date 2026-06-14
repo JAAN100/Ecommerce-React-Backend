2f35272630126f53f408b165bfbf27726ad28c26
Hassan jaan <hassan,jan,solo@gmail.com>
2026-06-14 18:41:40 +0500

    fxed

--- models/index.js
+++ models/index.js
@@ -1,18 +1,18 @@
 import { Sequelize } from 'sequelize';
-import sqlJsAsSqlite3 from 'sql.js-as-sqlite3';
 import fs from 'fs';
-
+ 
 const isUsingRDS = process.env.RDS_HOSTNAME && process.env.RDS_USERNAME && process.env.RDS_PASSWORD;
-const dbType = process.env.DB_TYPE || 'mysql';
+const dbType = process.env.DB_TYPE || 'postgres';
 const defaultPorts = {
   mysql: 3306,
   postgres: 5432,
 };
 const defaultPort = defaultPorts[dbType];
-
+ 
 export let sequelize;
-
+ 
 if (isUsingRDS) {
+  const { default: pg } = await import('pg');
   sequelize = new Sequelize({
     database: process.env.RDS_DB_NAME,
     username: process.env.RDS_USERNAME,
@@ -20,16 +20,24 @@ if (isUsingRDS) {
     host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT || defaultPort,
     dialect: dbType,
+    dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
     logging: false
   });
 } else {
+  const sqlJsAsSqlite3 = (await import('sql.js-as-sqlite3')).default;
   sequelize = new Sequelize({
     dialect: 'sqlite',
     dialectModule: sqlJsAsSqlite3,
     logging: false
   });
-
-  // Save database to file after write operations.
+ 
+  // Save database to file after write operations (local only)
   sequelize.addHook('afterCreate', saveDatabaseToFile);
   sequelize.addHook('afterDestroy', saveDatabaseToFile);
   sequelize.addHook('afterUpdate', saveDatabaseToFile);
@@ -39,7 +47,7 @@ if (isUsingRDS) {
   sequelize.addHook('afterBulkDestroy', saveDatabaseToFile);
   sequelize.addHook('afterBulkUpdate', saveDatabaseToFile);
 }
-
+ 
 export async function saveDatabaseToFile() {
   const dbInstance = await sequelize.connectionManager.getConnection();
   const binaryArray = dbInstance.database.export();
